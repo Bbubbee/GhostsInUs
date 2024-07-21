e677c834ceff65c224766f43d4fd597028bc8c81
Darren Borromeo <darrenborromeodrb@gmail.com>
2024-07-21 22:56:23 +1000

    basic typing

--- scenes/levels/level_manager/base_level.gd
+++ scenes/levels/level_manager/base_level.gd
@@ -7,9 +7,28 @@ class_name Level
 			* An autoload "Types" node with class "EnterParams" 
 """
 
-signal change_level(level_path: String, enter_params: Types.EnterParams)
+var current_word: String = "hello"
+var index: int = 0
 
+signal change_level(level_path: String, enter_params: Types.EnterParams)
 
 func init(enter_params: Types.EnterParams = null): 
 	if enter_params: 
 		print("Hey! I have enter params")
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		var typed_event = event as InputEventKey
+		var key_typed = PackedByteArray([typed_event.unicode]).get_string_from_utf8()
+		#print(key_typed)
+	
+		if key_typed == current_word[index]:
+			print("correct!: " + key_typed)
+			index = min(index+1, current_word.length()-1)
+			
+			
+		
+		
+		
+
