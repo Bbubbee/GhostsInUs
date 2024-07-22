2ef1dae93c72bac51fcc92dc61debff72c34c32a
bbubbee <darrenborromedrb@gmail.com>
2024-07-22 19:53:12 +1000

    transfer

--- scenes/levels/level_manager/base_level.gd
+++ scenes/levels/level_manager/base_level.gd
@@ -7,10 +7,18 @@ class_name Level
 			* An autoload "Types" node with class "EnterParams" 
 """
 
-var current_word: String = "hello"
+var current_line: String = "hello [color=red]sexy ass"
 var index: int = 0
 
 signal change_level(level_path: String, enter_params: Types.EnterParams)
+
+@onready var subtitle = $Subtitle
+
+
+
+func _ready():
+	subtitle.text = current_line
+
 
 func init(enter_params: Types.EnterParams = null): 
 	if enter_params: 
@@ -22,10 +30,15 @@ func _unhandled_input(event: InputEvent) -> void:
 		var typed_event = event as InputEventKey
 		var key_typed = PackedByteArray([typed_event.unicode]).get_string_from_utf8()
 		#print(key_typed)
-	
-		if key_typed == current_word[index]:
+		
+		# Typed the correct character. 
+		if key_typed == current_line[index]:
+			current_line[index+1] = '[/color]'
+			subtitle.text = current_line
 			print("correct!: " + key_typed)
-			index = min(index+1, current_word.length()-1)
+			
+			# Advance to the next character. 
+			index = min(index+1, current_line.length()-1)
 			
 			
 		
