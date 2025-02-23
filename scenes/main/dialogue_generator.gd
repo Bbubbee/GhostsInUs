c06ddbc20fe2249bca0eb0e1519649122f7768fa
bbubbee <darrenborromedrb@gmail.com>
2025-02-24 00:12:04 +1100

    transfer

--- scenes/main/dialogue_generator.gd
+++ scenes/main/dialogue_generator.gd
@@ -15,7 +15,10 @@ var current_question: int = 1
 @onready var options: Node2D = $Options
 
 var active_option  
+var active_string 
 
+# Index of the current char in the active_option
+var index: int = 0
 
 func _ready() -> void:
 	# Gets the dialogue json. 
@@ -23,33 +26,29 @@ func _ready() -> void:
 	var json = JSON.new() 
 	var data = json.parse_string(file.get_as_text())
 	current_character = data["character_1"]
-
 	
-func _input(event: InputEvent) -> void:
-	# Temporary generate dialogue option. 
-	if event.is_action_pressed("Generate Dialogue"): 
-		var i = 1
-		# Get all dialogue options
-		for option in current_character[str(current_question)]: 
-			# This is the question, not an option.
-			if str(option) == "question": continue
-			
-			var dialogue_string = current_character[str(current_question)][str(option)]
-			var dialogue_bubble = DIALOGUE_BUBBLE.instantiate()
-			match i: 
-				1: 
-					dialogue_bubble.init(dialogue_string, dialogue_marker_1.position)
-				2: 
-					dialogue_bubble.init(dialogue_string, dialogue_marker_2.position) 
-				3: 
-					dialogue_bubble.init(dialogue_string, dialogue_marker_3.position) 
-				4: 
-					dialogue_bubble.init(dialogue_string, dialogue_marker_4.position) 
-				_: 
-					dialogue_bubble.init(dialogue_string, dialogue_marker_1.position)
-					
-			i+=1
-			options.add_child(dialogue_bubble)
+	var i = 1
+	# Get all dialogue options
+	for option in current_character[str(current_question)]: 
+		# This is the question, not an option.
+		if str(option) == "question": continue
+		
+		var dialogue_string = current_character[str(current_question)][str(option)]
+		var dialogue_bubble = DIALOGUE_BUBBLE.instantiate()
+		match i: 
+			1: 
+				dialogue_bubble.init(dialogue_string, dialogue_marker_1.position)
+			2: 
+				dialogue_bubble.init(dialogue_string, dialogue_marker_2.position) 
+			3: 
+				dialogue_bubble.init(dialogue_string, dialogue_marker_3.position) 
+			4: 
+				dialogue_bubble.init(dialogue_string, dialogue_marker_4.position) 
+			_: 
+				dialogue_bubble.init(dialogue_string, dialogue_marker_1.position)
+				
+		i+=1
+		options.add_child(dialogue_bubble)
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -62,10 +61,19 @@ func _unhandled_input(event: InputEvent) -> void:
 		if not active_option: 
 			for option in options.get_children(): 
 				if key_typed == option.text[0]: 
-					active_option = option
-					print("current option = ", option.text)
+					active_option = option	
+					active_string = active_option.text 
+					active_option.text = Globals.set_subtitle(index, active_string)
+					index = min(index+1, active_option.text.length())
 		else: 
-			print("typing: ", key_typed)
+			# Typed the correct character
+			if key_typed == active_string[min(index, active_string.length()-1)]:
+				## Advance to the next character
+				active_option.text = Globals.set_subtitle(index, active_string)
+				index = min(index+1, active_string.length())
+				
+
+
 				
 		
 			
--- utils/globals.gd
+++ utils/globals.gd
@@ -7,16 +7,16 @@ extends Node
 func set_subtitle(
 	index, 
 	text, 
-	before_color: Color = Color("#4682b4"), 
-	current_color: Color = Color("#4682b4"), 
-	after_color: Color = Color("#4682b4")
+	before_color: Color = Color.YELLOW, 
+	current_color: Color = Color.YELLOW, 
+	after_color: Color = Color.WHITE
 ):
 	# Correct chars. 
 	var done_chars = get_bbcode_color_tag(before_color) + text.substr(0, index) + get_bbcode_color_tag()
 	# Current char. 
 	var current_chars = get_bbcode_color_tag(current_color) + text.substr(index, 1) + get_bbcode_color_tag()
 	# Chars left to type.  
-	var todo_chars = get_bbcode_color_tag(after_color) + text.substr(index+1, text.length()-1) + get_bbcode_color_tag()
+	var todo_chars = get_bbcode_color_tag(after_color) + "[u]" + text.substr(index+1, 1) + "[/u]" + text.substr(index+2, text.length()-1) + get_bbcode_color_tag()
 	return set_center_tags(done_chars+current_chars+todo_chars)		
 		
 		
